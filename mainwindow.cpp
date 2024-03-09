a67a073813d545d0fa77240d766565fa5267d8de
Emma Eastland <127785795+eastland-emma@users.noreply.github.com>
2024-03-08 23:45:33 -0700

    Reuploaded the updated files from work on days 3/7 and 3/8
    Added logic for adding to the existing string as well as checking user correctness. 
    Fixed formatting on the connect operator for the light display methods to fit modern convention.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,28 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+#include <random>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //int next = std::rand()%2;
+    //char nextChar = next + 48;//Converts the int zero to 48 or the int one to 49 for ascii usage
+
+    //Set up the input strings for the simon cpu and the user
+    //simonsSeq+=nextChar;
+
+    simonsSeq = "0001010111";  //TESTING PURPOSES ONLY: I AM USING THIS TO TEST THE SIMON LIGHT UP FEATURE
+    usersSeq="";
+
+    //Set up timer to trigger the simon light up effect
+    timer = new QTimer(this);
+    connect(timer,
+            &QTimer::timeout,
+            this,
+            &MainWindow::scheduledLightCallback);
 }
 
 MainWindow::~MainWindow()
@@ -23,12 +40,106 @@ void MainWindow::on_startButton_clicked()
 
 void MainWindow::on_redButton_clicked()
 {
+    //Used to test button responsiveness
+    //TODO: Remove before turn in
     ui->testSeqLabel->setText(ui->testSeqLabel->text()+= '0');
+    usersSeq += '0';
+    usersSeqIndex++;
+    //Checks if the user clicked the right button
+    if(usersSeq[usersSeqIndex]==simonsSeq[usersSeqIndex])
+    {
+        //If the last button pressed was the end of the pattern
+        if(static_cast<int>(simonsSeq.size())== usersSeqIndex)
+        {//Successful run with patern
+            //Add to the pattern
+            simonsSeq = add_to_pattern(simonsSeq);
+            //Call the simon Pattern Display
+            //remove the users' pattern so far
+        }
+        return;
+    }
+    else
+    {
+        //Game over pop-up
+        return;
+    }
 }
 
 
 void MainWindow::on_blueButton_clicked()
 {
     ui->testSeqLabel->setText(ui->testSeqLabel->text()+= '1');
+    usersSeq += '1';
+    usersSeqIndex++;
+    //Checks if the user clicked the right button
+    if(usersSeq[usersSeqIndex]==simonsSeq[usersSeqIndex])
+    {
+        //If the last button pressed was the end of the pattern
+        if(static_cast<int>(simonsSeq.size())== usersSeqIndex)
+        {//Successful run with patern
+            //Add to the pattern
+            simonsSeq = add_to_pattern(simonsSeq);
+            //Call the simon Pattern Display
+            //remove the users' pattern so far
+        }
+        return;
+    }
+    else
+    {
+        //Game over pop-up
+        return;
+    }
+}
+
+
+void MainWindow::playSequence()
+{
+    ui->redButton->setEnabled(false);
+    ui->blueButton->setEnabled(false);
+    simonsSeqIndex = 0;
+    timer->start(500);
+
+}
+
+void MainWindow::scheduledLightCallback()
+{
+
+    if(simonsSeqIndex >= simonsSeq.length())
+    {
+        timer->stop();
+        std::cout<<"finished";
+        return;
+    }
+    std::cout<<simonsSeq[simonsSeqIndex];
+    if(simonsSeq[simonsSeqIndex]=='0')
+    {
+        //light redButton
+        ui->redLight->setEnabled(true);
+        ui->redLight->setEnabled(false);
+    }
+    else
+    {
+        //light blue button
+        ui->blueButton->setEnabled(true);
+        ui->blueButton->setEnabled(false);
+    }
+    simonsSeqIndex++;
+}
+
+// //Adds a 1 or 0 at the end of the string (pattern) as the player is advancing through
+std::string MainWindow::add_to_pattern(std::string currentPattern)
+{
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> distribution(0, 1);
+    int random_number = distribution(rng);
+    std::string newRandomGeneratedChar = std::to_string(random_number);
+    currentPattern = currentPattern + newRandomGeneratedChar;
+    return currentPattern;
+}
+
+void MainWindow::on_seqTestButton_clicked()
+{
+    playSequence();
 }
 
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -18,13 +19,40 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_startButton_clicked();
-
+    ///@brief adds a red indicator to the user's sequence and compares against the simon function to see if it is correct
     void on_redButton_clicked();
-
+    ///@brief adds a blue indicator to the user's sequence and compares against the simon function to see if it is correct
     void on_blueButton_clicked();
+    ///@brief starts the game logic
+    void on_startButton_clicked();
+    ///REMOVE BEFORE TURN IN
+    void on_seqTestButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    //Holds the current index of the simon sequence. Used in the play sequence and scheduled light callback methods
+    int simonsSeqIndex;
+    //Holds the index we are comparing on the user's sequence
+    int usersSeqIndex;
+
+    //Holds the correct simon sequence
+    std::string simonsSeq;
+    //Holds the user's simon sequence
+    std::string usersSeq;
+
+    //Controls time for the simon light function
+    QTimer* timer;
+
+    //private helpers
+
+    ///@brief temporarily disables the users buttons and plays the current simon sequence to the user in 1 second intervals
+    void playSequence();
+
+    ///@brief called by the timer object in order to light once every second
+    void scheduledLightCallback();
+
+    ///@brief adds a new character, either '1' or '0', to the existing simon's sequence
+    std::string add_to_pattern(std::string currentPattern);
 };
 #endif // MAINWINDOW_H
