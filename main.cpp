73e0d7410d8d0f1a5d0a1aef12c5bf7642f3d1fa
Emma Eastland <127785795+eastland-emma@users.noreply.github.com>
2024-03-12 22:44:52 -0600

    Finished base functionality (NO MVC)
    Fixed bug related to userSequence validation
    Fixed lights display
    Added WASD support
    Added <^v> support

--- main.cpp
+++ main.cpp
@@ -5,11 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow emitter;
-    Model receiver;
-    QObject::connect(&emitter, &MainWindow::on_redButton_clicked_signal, &receiver, &Model::handle_redButton_signal);
-    QObject::connect(&emitter, &MainWindow::on_blueButton_clicked_signal, &receiver, &Model::handle_blueButton_signal);
-    QObject::connect(&emitter, &MainWindow::on_startButton_clicked_signal, &receiver, &Model::handle_startButton_signal);
-    emitter.show();
+    MainWindow w;
+    w.show();
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,19 +3,24 @@
 #include <iostream>
 #include <random>
 
-MainWindow::MainWindow( QWidget *parent)
+MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
     //Set up the input strings for the simon cpu and the user
+    simonsSeq = "";
+    usersSeq="";
+
+    usersSeqIndex=0;
     //Set up timer to trigger the simon light up effect
     timer = new QTimer(this);
     connect(timer,
             &QTimer::timeout,
             this,
             &MainWindow::scheduledLightCallback);
+    count = 0;
 }
 
 MainWindow::~MainWindow()
@@ -28,35 +33,113 @@ void MainWindow::on_startButton_clicked()
     ui->blueButton->setEnabled(true);
     ui->redButton->setEnabled(true);
     ui->startButton->setEnabled(false);
-    emit on_startButton_clicked_signal();
+    add_to_pattern();
+    playSequence();
 }
-
 
 void MainWindow::on_redButton_clicked()
 {
-    emit on_redButton_clicked_signal();
-    std::cout<<"signal sent for red button"<<std::endl;
+    //Used to test button responsiveness
+    //TODO: Remove before turn in
+    ui->testSeqLabel->setText(ui->testSeqLabel->text()+= '0');
+    usersSeq += '0';
+    std::cout<<"Your Sequence: "<<usersSeq<<"\t"<<"correct Sequence: "<<simonsSeq<<std::endl;
+    //Checks if the user clicked the right button
+    if(simonsSeq.at(usersSeqIndex)== usersSeq.at(usersSeqIndex))
+    {
+        std::cout<<simonsSeq.size()<<"    "<<usersSeqIndex+1<<std::endl;
+        //If the last button pressed was the end of the pattern
+        if(static_cast<int>(simonsSeq.size())== usersSeqIndex+1)
+        {//Successful run with patern
+            //Add to the pattern
+            add_to_pattern();
+            //remove the users' pattern so far
+            usersSeq="";
+            usersSeqIndex =0;
+            //Call the simon Pattern Display
+            playSequence();
+            ui->testSeqLabel->setText("UsersSeq: ");
+            return;
+        }
 
+        usersSeqIndex++;
+        return;
+    }
+    else
+    {
+        ui->blueButton->setEnabled(false);
+        ui->redButton->setEnabled(false);
+        ui->startButton->setEnabled(true);
+        simonsSeq="";
+        usersSeq="";
+        usersSeqIndex = 0;
+        simonsSeqIndex = 0;
+        //Game over pop-up
+        return;
+    }
 }
 
 void MainWindow::on_blueButton_clicked()
 {
-    emit on_blueButton_clicked_signal();
-    std::cout<<"signal sent for blue button"<<std::endl;
+    ui->testSeqLabel->setText(ui->testSeqLabel->text()+= '1');
+    usersSeq += '1';
+    std::cout<<"Your Sequence: "<<usersSeq<<"\t"<<"correct Sequence: "<<simonsSeq<<std::endl;
+    //Checks if the user clicked the right button
+    if(simonsSeq.at(usersSeqIndex) == usersSeq.at(usersSeqIndex))
+    {
+        std::cout<<simonsSeq.size()<<"    "<<usersSeqIndex+1<<std::endl;
+        //If the last button pressed was the end of the pattern
+        if(static_cast<int>(simonsSeq.size())== usersSeqIndex+1)
+        {//Successful run with patern
+            //Add to the pattern
+            add_to_pattern();
+            //remove the users' pattern so far
+            usersSeq="";
+            usersSeqIndex =0;
+            //Call the simon Pattern Display
+            playSequence();
+            //increase score
+            ui->testSeqLabel->setText("UsersSeq: ");
+            return;
+        }
 
+        usersSeqIndex++;
+        return;
+    }
+    else
+    {
+        ui->blueButton->setEnabled(false);
+        ui->redButton->setEnabled(false);
+        ui->startButton->setEnabled(true);
+        simonsSeq ="";
+        usersSeq = "";
+        usersSeqIndex = 0;
+        simonsSeqIndex = 0;
+        //Game over pop-up
+        return;
+    }
 }
 
-void MainWindow::on_seqTestButton_clicked()
+void MainWindow::playSequence()
 {
-    emit on_seqTestButton_clicked_signal();
-    std::cout<<"signal sent for seqTest button"<<std::endl;
+
+    std::cout<<"New Sequence: "<<simonsSeq<<std::endl;
+    ui->redButton->setEnabled(false);
+    ui->blueButton->setEnabled(false);
+    simonsSeqIndex = 0;
+    timer->start(500);
 
 }
 
 void MainWindow::scheduledLightCallback()
 {
-    ui->redButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(119,119,119);} QPushButton{border:none;background-color:rgb(119, 119, 119);border-radius:85px;}} QPushButton:enabled{border:none; background-color:rgb(255, 0, 0); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 119);}"));
-    ui->blueButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(119,119,119);} QPushButton{border:none;background-color:rgb(119, 119, 119);border-radius:85px;}} QPushButton:enabled{border:none; background-color:rgb(0, 0, 255); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 119);}"));
+    if(count%2==0)
+    {
+        ui->redButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(255,119,119);} QPushButton{border:none;background-color:rgb(119, 119, 119);border-radius:85px;} QPushButton:enabled{border:none; background-color:rgb(255, 0, 0); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 119);}"));
+        ui->blueButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(119,119,255);} QPushButton{border:none;background-color:rgb(119, 119, 119);border-radius:85px;} QPushButton:enabled{border:none; background-color:rgb(0, 0, 255); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 119);}"));
+        count++;
+        return;
+    }
     if(simonsSeqIndex >= simonsSeq.length())
     {
         timer->stop();
@@ -64,18 +147,53 @@ void MainWindow::scheduledLightCallback()
         ui->redButton->setEnabled(true);
         ui->blueButton->setEnabled(true);
         return;
+        count =0;
     }
     if(simonsSeq[simonsSeqIndex]=='0')
     {
         //light redButton
-        ui->redButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(255,0,0);} QPushButton{border:none;background-color:rgb(255, 119, 119);border-radius:85px;}} QPushButton:enabled{border:none; background-color:rgb(255, 0, 0); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(255, 119, 119);}"));
-        ui->blueButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(119,119,255);} QPushButton{border:none;background-color:rgb(119, 119, 255);border-radius:85px;}} QPushButton:enabled{border:none; background-color:rgb(0, 0, 255); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 255);}"));
+        ui->redButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(255,0,0);} QPushButton{border:none;background-color:rgb(255, 119, 119);border-radius:85px;} QPushButton:enabled{border:none; background-color:rgb(255, 0, 0); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(255, 119, 119);}"));
+        ui->blueButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(119,119,255);} QPushButton{border:none;background-color:rgb(119, 119, 255);border-radius:85px;} QPushButton:enabled{border:none; background-color:rgb(0, 0, 255); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 255);}"));
     }
     else
     {
         //light blue button
-        ui->redButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(255,119,119);} QPushButton{border:none;background-color:rgb(255, 119, 119);border-radius:85px;}} QPushButton:enabled{border:none; background-color:rgb(255, 0, 0); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(255, 119, 119);}"));
-        ui->blueButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(0,0,255);} QPushButton{border:none;background-color:rgb(119, 119, 255);border-radius:85px;}} QPushButton:enabled{border:none; background-color:rgb(0, 0, 255); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 255);}"));
+        ui->redButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(255,119,119);} QPushButton{border:none;background-color:rgb(255, 119, 119);border-radius:85px;} QPushButton:enabled{border:none; background-color:rgb(255, 0, 0); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(255, 119, 119);}"));
+        ui->blueButton->setStyleSheet( QString("QPushButton::!enabled {background-color: rgb(0,0,255);} QPushButton{border:none;background-color:rgb(119, 119, 255);border-radius:85px;} QPushButton:enabled{border:none; background-color:rgb(0, 0, 255); border-radius:85px;} QPushButton:pressed {border-radius:70px; background-color:rgb(119, 119, 255);}"));
     }
+    count++;
     simonsSeqIndex++;
+}
+
+void MainWindow::add_to_pattern()
+{
+    std::random_device rd;
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> distribution(0, 1);
+    int random_number = distribution(rng);
+    std::string newRandomGeneratedChar = std::to_string(random_number);
+    simonsSeq += newRandomGeneratedChar;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    //Support for Arrow Keys
+    if(event->key() == Qt::Key_Right)
+    {
+        on_blueButton_clicked();
+    }
+    if(event->key() == Qt::Key_Left)
+    {
+        on_redButton_clicked();
+    }
+
+    //Support for WASD
+    if(event->key() == Qt::Key_A)
+    {
+        on_redButton_clicked();
+    }
+    if(event->key() == Qt::Key_D)
+    {
+        on_blueButton_clicked();
+    }
 }
